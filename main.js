08bbd9e69ad6bb559f7025cd74a8cdda17a3882f
Stanislav Simahin <stopikytub@gmail.com>
2021-07-01 20:21:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,18 @@
-const RPC = require('discord-rpc');
+const RPC = require("discord-rpc");
+const rpc = new RPC.Client({transport: "ipc"});
 
-const rpc = new RPC.Client({
-    transport: "ipc"
-});
-
-rpc.on('ready', () => {
+rpc.on("ready", () => {
     rpc.setActivity({
-        details: "Тестирование", // 2 текст после названия приложения
-        state: "Привет!", // 3 текст после названия приложения
+        details: "Text",
+        state: "Too text!",
         startTimestamp: new Date(),
-        largeImageKey: "image", // в "" вводим название большой картинки, которую загрузили
-        largeImageText: "Ты это нашел!", // текст когда ты наводишься на большую картинку
+        largeImageKey: "python", 
+        largeImageText: "Hmm!",
 
-        smallImageKey: "image2", // в "" вводим название маленькой картинки, которую загрузили
-        smallImageText: "Вау!" // текст когда ты наводишься на маленькую картинку
+        smallImageKey: "bot_developer", 
+        smallImageText: "Wow!"
     });
     
-    console.log(`RPC успешно загружён!`)
 })
 
-rpc.login({
-    clientId: "" // сюда вводим клиент айди приложения
-})
+rpc.login({clientId: "792818909844996139" }) // insert here your app id
